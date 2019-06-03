1eb21321578d3102bfa95b7f06429db622755da6
Old Wan <36687023+CPSparrow@users.noreply.github.com>
2019-06-03 16:48:15 +0800

    Add files via upload
    万事开头难

---
+++ Gets-Up.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int part=0;
+
+//which wepon
+const char wep[10][5]={
+" "," "," "," "," "," "," "," "," "," "," "
+};
+
+struct{
+	unsigned short has;
+	unsigned short aim;
+	bool isBe;
+	unsigned short loca;
+	bool live;
+};
+
+int main(){
+	return 0;
+}
