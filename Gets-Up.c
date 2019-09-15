ab85133d598065f6ddd6337e57633013044ce2cb
Old Wan <36687023+CPSparrow@users.noreply.github.com>
2019-09-15 10:06:18 +0800

    把Puding改成gets_up（其实两者区别不大）

--- Gets-Up.c
+++ Gets-Up.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
-#include<windows.h>
 #include <stdbool.h>
+#include<time.h>
+#include<windows.h>
 
-#define places (8)
+bool isStart=0;
 
-int part=0;
-
-//which wepon
 const char wep[10][10]= {
 	"fits","gun","plane","tank","morto",
 	"martoon","tomato","knife","iPad","egg"
+};
+
+const char place[9][15]= {
+	"empty","man 0's home","man 1's home",
+	"market 1","market 2","street 1",
+	"street 2","park 1","park 2", 
 };
 
 struct model {
@@ -22,8 +24,9 @@ struct model {
 	bool isBe;
 } man[2];
 
+
 bool chos() {
-	srand(time(NULL));
+	srand(0/*time(NULL)*/);
 	if(rand()%2) {
 		return 0;
 	} else {
@@ -38,553 +41,330 @@ void p(int n) {
 	}
 }
 
-int setAc(bool ac) {
-	unsigned short  re;
-	printf("now is man %d\n",ac+1);
+void stay(){
+	printf("--------------------------------\n");
+	printf("we will stay for two seconds. . .\n");
+	Sleep(2000); 
+}
+
+void pInfo(){
+	printf("man 0 has a %s",wep[man[0].has]);
+	p(19);
+	printf("man 1 has a %s\n",wep[man[1].has]);
+	
+	printf("man 0 is now at %s",place[man[0].loca]);
+	p(7);
+	printf("man 1 is now at %s\n",place[man[1].loca]);
+	
+	switch(man[0].situ){
+		case 1:
+			printf("man 0 is normol");
+			break;
+		case 2:
+	 		printf("man 0 is asleep");
+			break;
+		case 3:
+			printf("man 0 is trapped");
+			break;
+		case 4:
+			printf("man 0 is asleep and trapped");
+			break;
+		default:
+			printf("something wrong in pInfo() -> 0");
+			printf("please tell the developer");
+			break;
+	}
+	
+	p(20);
+	
+	switch(man[1].situ){
+		case 1:
+			printf("man 1 is normol\n");
+			break;
+		case 2:
+	 		printf("man 1 is asleep\n");
+			break;
+		case 3:
+			printf("man 1 is trapped\n");
+			break;
+		case 4:
+			printf("man 1 is asleep and trapped\n");
+			break;
+		default:
+			printf("something wrong in pInfo()");
+			printf("please tell the developer\n\n");
+			break;
+	}
+	
+	if(man[0].aim==0){
+		printf("man 0 has not got an aim");
+	}else{
+		printf("man 0 has got an aim");
+	}
+	
+	p(11);
+	
+	if(man[1].aim==0){
+		printf("man 1 has not got an aim\n");
+	}else{
+		printf("man 1 has got an aim\n");
+	}
+	
+	if(man[0].isBe==0){
+		printf("man 0 is not an aim now");
+	}else{
+		printf("man 0 is an aim now");
+	}
+	
+	p(12);
+	
+	if(man[1].isBe==0){
+		printf("man 1 is not an aim now\n");
+	}else{
+		printf("man 1 is an aim now\n");
+	}
+	
+	printf("\n");
+}
+
+int setAc_new(bool ac) {
+
+	////////////////////////////////////////
+	unsigned int re=1;
+	unsigned short ct=1;
+	//system("cls");
+	if(isStart==1){
+		printf("\n\n\n");
+	}else{
+		isStart=1;
+	}
+	pInfo(ac);
+	printf("now is man %d's turn\n",ac);
+	printf("please choose your action\n\n");
+	////////////////////////////////////////
+	
 	if(man[ac].situ==2 || man[ac].situ==4) {
+
 		printf("1.wake");
 		re=0;
+
 	} else if(man[ac].situ==3) {
-		printf("1.sleep  	");
-		//p(14);
-		printf("2.jie	");
+
+		printf("1. sleep    ");
+		printf("2.jie");
 		re=1;
+
 	} else if(man[ac].situ==1) {
-		printf("1.move   ");
-		//p(14);
+
+		printf("1. move   ");
+		re*=10;
+		//re=10
+
+		printf("2. sleep  ");
+		re+=1;
+		re*=10;
+		//re=110
+		
+		ct=3;
+
 		if(man[ac].isBe==1) {
-			if(man[ac].aim==1) {
-				if(man[ac].loca==5) {
-					printf("2.hide	  ");
-					//p(12);
-					printf("3.fire	  ");
-					//p(14);
-					printf("4.change  ");
-					//p(12);
-					printf("5.sleep	  ");
-					//p(12);
-					printf("6.bang	  ");
-					re=2;
-				} else {
-					printf("2.hide	  ");
-					//p(14);
-					printf("3.fire	  ");
-					//p(14);
-					printf("4.sleep	  ");
-					//p(12);
-					printf("")
-					re=3;
-				}
-			} else {
-				if(man[ac].loca==5) {
-					printf("2.hide		");
-					//p(12);
-					printf("3.aim		");
-					//p(14);
-					printf("4.change	");
-					//p(14);
-					printf("5.sleep		");
-					re=4;
-				} else {
-					printf("2.hide		");
-					//p(14);
-					printf("3.aim		");
-					//p(14);
-					printf("4.sleep		");
-					re=5;
-				}
-			}
-		} else {//Î´±»Ãé×¼
-			if(man[ac].aim==1) {
-				if(man[ac].loca==5) {
-					printf("2.fire		");
-					//p(14);
-					printf("3.change	");
-					//p(14);
-					printf("4.sleep		");
-					re=6;
-				} else {
-					printf("2.fire		");
-					//p(14);
-					printf("3.sleep		");
-					re=7;
-				}
-			} else {
-				if(man[ac].loca==5) {
-					printf("2.aim		");
-					//p(15);
-					printf("3.change	");
-					//p(14);
-					printf("4.sleep		");
-					re=8;
-				} else {
-					printf("2.aim		");
-					//p(14);
-					printf("3.sleep");
-					re=9;
-				}
+			printf("%d. hide   ",ct++);
+			re+=1;
+		}
+		re*=10;
+
+		if(man[ac].aim==1) {
+		    printf("%d. fire   ",ct++);
+			re+=1;
+		}
+		re*=10; 
+		
+		if(man[ac].loca==3||man[ac].loca==4){
+			printf("%d. change ",ct++);
+			re+=1;
+		}
+		re*=10;
+		
+		if(man[0].loca==man[1].loca){
+			printf("%d. bang   ",ct++);
+			re+=1;
+		}
+		re*=10;
+		
+		if(man[ac].aim==0){
+			printf("%d. aim    ",ct++);
+			re+=1;
+		}
+		
+	} else {
+		printf("wrong!!! -> 110\n");
+		return -1;
+	}
+	
+	printf("\n");
+	
+	return re;
+
+}
+
+void fight_new(){
+	//////////////////////////////
+	int ac=chos();
+	unsigned int re=setAc_new(ac);
+	//////////////////////////////
+	
+	/////////////////
+	unsigned short b;
+	/////////////////
+	
+	///////////////
+	unsigned int a;
+	fflush(stdin);
+	scanf("%d",&a);
+	fflush(stdin);
+	///////////////
+	
+	////////////////////////////////
+	printf("re = %d\n",re);
+	if(a>=3){
+		unsigned int x,y=re,z=10000;
+		for(x=1;x<=4;x++){
+			if(y/z%10==0&&a>=x+2){
+				a+=1;
+				//printf("a++\n");
+				z/=10;
 			}
 		}
 	}
-	printf("\n");
-	return re;
-}
-
-void fight() {
-
-	int ac=chos();
-	int i = setAc(ac);
-
-	char a;
-	scanf("%c",&a);
-	fflush(stdin);
-
-	switch(i) {
-
+	////////////////////////////////
+	
+	//////////////////////////////////////////////////////////////////
+	switch(re){
+		case -1:
+			printf("there is some thing wrong in setAc()\n\n");
+			printf("please stop to tell the developer immediately\n");
+			break;
+			
 		case 0:
-			if(a==1) {
+			if(a==1){
 				man[ac].situ-=1;
-				printf("man[%d] is awake now\n",ac+1);
-			} else {
-				printf("a wrong input!!!\n");
+				printf("you are awake\n\n");
+			}else{
+				printf("wrong input!!\n\n");
 			}
 			break;
-
+			
 		case 1:
-			if(a==1) {
+			if(a==1){
 				man[ac].situ-=2;
-				printf("man[%d] is free now\n",ac+1);
-			} else if(a==2) {
+				printf("escaped from ropes\n\n");
+			}else if(a==2){
 				man[ac].situ+=1;
-				printf("man[%d] is asleep\n",ac+1);
-			} else {
-				printf("a wrong input!!!\n");
+				man[ac].loca=ac;
+				man[ac].loca+=1;
+				printf("back to bed\n\n");
+			}else{
+				printf("wrong input!!!\n\n");
 			}
 			break;
-
-		case 2:
-			switch(a) {
-
+			
+		default:
+			
+			switch(a){
+				
+				//move
 				case 1:
 					srand(time(NULL));
-					int b;
-					b=rand()%places;
+					b=rand()%7;
 					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
+					if(b==man[ac].loca){
+						b*=b;
+						b*=b;
+						b%=7;
+						b+=1;
 					}
 					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-					if(ac==0) {
-						man[1].aim=0;
-					} else {
-						man[0].aim=0;
-					}
-					printf("safe\n");
-					break;
-
-				case 3:
-
-					if(ac==0) {
-						man[1].situ=0;
-						printf("man 1 died\n");
-					} else {
-						man[0].aim=0;
-						printf("man 2 died\n");
-					}
-					break;
-
-				case 4:
-					srand(time(NULL));
-					b=rand()%10;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].has) {
-							b*=b;
-							b%=10;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					printf("wepon No.%d\n",b);
-					break;
-
-				case 5:
-					man[ac].situ+=1;
-					printf("asleep now\n");
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-
-		case 3:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-					if(ac==0) {
-						man[1].aim=0;
-					} else {
-						man[0].aim=0;
-					}
-					printf("safe\n");
-					break;
-
-				case 3:
-					if(ac==0) {
-						man[1].situ=0;
-						printf("man 1 died\n");
-					} else {
-						man[0].aim=0;
-						printf("man 2 died\n");
-					}
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-
-		case 4:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-					if(ac==0) {
-						man[1].aim=0;
-					} else {
-						man[0].aim=0;
-					}
-					printf("safe\n");
+					man[0].isBe=man[1].isBe=0;
+					man[0].aim=man[1].aim=0;
+					printf("at %s now\n\n",place[b]);
 					break;
 					
+				//sleep
+				case 2:
+					man[ac].situ+=1;
+					man[ac].aim=0;
+					printf("asleep\n\n");
+					break;
+				
+				//hide	
 				case 3:
-					man[ac].aim=1;
-					printf("got aim\n");
+					man[0].isBe=man[1].isBe=0;
+					man[0].aim=man[1].aim=0;
+					printf("out of aim\n\n");
 					break;
-
+				
+				//fire	
 				case 4:
-					srand(time(NULL));
-					b=rand()%10;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].has) {
-							b*=b;
-							b%=10;
-							b+=1;
-						} else {
-							break;
-						}
+					if(ac==0){
+						man[1].situ=0;
+						printf("man 1 died\n\n");
+					}else{
+						man[0].situ=0;
+						printf("has man 0 killed\n\n");
 					}
-					printf("wepon No.%d\n",b);
 					break;
-
+					
+				//change	
 				case 5:
-					man[ac].situ+=1;
-					printf("asleep now\n");
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-		case 5:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-				case 2:
-					if(ac==0) {
-						man[0].isBe=man[1].aim=0;
-					} else {
-						man[0].aim=man[1].isBe=0;
-					}
-					printf("man[%d] is safe\n",ac+1);
-					break;
-
-				case 3:
-					man[ac].situ+=1;
-					printf("asleep now\n");
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-
-		case 6:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-
-					if(ac==0) {
-						man[1].situ=0;
-						printf("man 1 died\n");
-					} else {
-						man[0].aim=0;
-						printf("man 2 died\n");
-					}
-					break;
-
-				case 3:
 					srand(time(NULL));
 					b=rand()%10;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].has) {
-							b*=b;
-							b%=10;
-							b+=1;
-						} else {
-							break;
-						}
+					if(b==man[ac].loca){
+						b+=1;
+						b%=10;
 					}
-					printf("wepon No.%d\n",b);
+					man[ac].has=b;
+					printf("have %s\n\n",wep[b]);
 					break;
-
-				case 4:
-					man[ac].situ+=1;
-					printf("asleep now\n");
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-
-		case 7:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-
-					if(ac==0) {
-						man[1].situ=0;
-						printf("man 1 died\n");
-					} else {
+				
+				//bang	
+				case 6:
+					if(ac==0){
+						man[1].situ+=2;
+						man[0].isBe=0;
+						man[1].aim=0;
+						printf("man 1 is trapped\n\n");
+					}else{
+						man[0].situ+=2;
+						man[1].isBe=0;
 						man[0].aim=0;
-						printf("man 2 died\n");
+						printf("man 1 is trapped\n\n");
 					}
 					break;
-
-				case 3:
-					man[ac].situ+=1;
-					printf("asleep now\n");
+					
+				case 7:
+					if(ac==0){
+						man[0].aim=1;
+						man[1].isBe=1;
+						printf("got a man 1\n\n");
+					}else{
+						man[1].aim=1;
+						man[0].isBe=1;
+						printf("got a man 0\n\n");
+					}
 					break;
-
-				default:
-					printf("a wrong input!!!\n");
+					
+				default :
+					printf("tell the developer wrong in fight_new");
+					printf(" -> first switch -> second switch\n\n");
 					break;
-
 			}
-			break;
-		case 8:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-
-					if(ac==0) {
-						man[1].situ=0;
-						printf("man 1 died\n");
-					} else {
-						man[0].aim=0;
-						printf("man 2 died\n");
-					}
-					break;
-
-				case 3:
-					srand(time(NULL));
-					b=rand()%10;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].has) {
-							b*=b;
-							b%=10;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					printf("wepon No.%d\n",b);
-					break;
-
-				case 4:
-					man[ac].situ+=1;
-					printf("asleep now\n");
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-		case 9:
-			switch(a) {
-
-				case 1:
-					srand(time(NULL));
-					int b;
-					b=rand()%places;
-					b+=1;
-					for(;;) {
-						if(b==man[ac].loca) {
-							b*=b;
-							b%=places;
-							b+=1;
-						} else {
-							break;
-						}
-					}
-					man[ac].loca=b;
-					printf("place No. %d\n",b);
-					break;
-
-				case 2:
-					man[ac].aim=1;
-					printf("got aim\n");
-					break;
-
-				case 3:
-					man[ac].situ+=1;
-					printf("asleep now\n");
-					break;
-
-				default:
-					printf("a wrong input!!!\n");
-					break;
-
-			}
-			break;
-		default:
-			printf("something wrong in fight() > first switch\n");
 			break;
 	}
-
+	//////////////////////////////////////////////////////////////////
+	
+	//
+	stay();
+	//
 }
 
 int main() {
@@ -592,14 +372,13 @@ int main() {
 	man[0].has=man[1].has=0;
 	man[0].aim=man[1].aim=0;
 	man[0].isBe=man[1].isBe=0;
-	man[0].situ=man[1].situ=1;
+	man[0].situ=man[1].situ=2;
 	man[0].loca=1;
 	man[1].loca=2;
 	//set for start
-
+	
 	while(man[0].situ!=0 && man[2].situ!=0) {
-		fight();
+		fight_new();
 	}
-
 	return 0;
 }
