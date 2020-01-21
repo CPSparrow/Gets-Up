ecb346444683ec471740686b969059c6716d7efa
Old Wan <36687023+CPSparrow@users.noreply.github.com>
2020-01-21 21:50:25 +0800

    无敌的输入程序

--- TEST/getChoice( )/getChoice.c
+++ TEST/getChoice( )/getChoice.c
@@ -4,12 +4,6 @@
 //  remain question :
 //  not solve a input like 1234abcd succcessfully
 
-//  (probably input)
-//  1,2,3...
-//  a,b,c... (10 times)
-//  1234abcd
-//  123 dsuhfiu2b389fn sdn dkfn s
-//  jq ef93bb js a
 #include<stdio.h>
 
 #define QUIT -1
@@ -18,10 +12,11 @@
 
 #define INTRODUCE 2
 
-#define CHANGECOLOR 3
-#define TOOMUCH "too much wrong input"
+#define CHANGE_COLOR 3
 
-#define TYPEAINT "please type a proper int like 1 or 2"
+#define TOO_MUCH "too much wrong input"
+
+#define TYPE_A_INT "please type a proper int like 1 or 2"
 
 /*/////////////////////////////////////////////////
 ///////these above are which is OK now.////////////
@@ -33,7 +28,7 @@ void clean(void){
 	}
 }
 
-int getChoice(void){
+/*int getChoice(void){
     unsigned short times;
     unsigned int choice,ch;
     times=choice=ch=0;
@@ -68,28 +63,53 @@ int getChoice(void){
             }
         }
     }
+}*/
+
+int new_getchoice(void){
+	unsigned short times=0;
+	while(1){
+		if(10==times){
+			printf("quit successfully\n");
+			return QUIT;
+		}else if(0<=times&&10>times){
+			times++;
+			const char first = getchar();
+			if('\n'==first){
+				times--;
+				continue;
+			}
+			const char second = getchar();
+			if('\n'==second){
+				switch(first){
+					case '0'+GAME:
+						return GAME;
+						//break;
+					case '0'+INTRODUCE:
+						return INTRODUCE;
+					case 'C':
+						return CHANGE_COLOR;
+					case 'q':
+					case 'Q':
+						return QUIT;
+					default:
+						//clean();
+						printf("%s\n",TYPE_A_INT);
+						break;
+				}
+			}else{
+				clean();
+				printf("%s\n",TYPE_A_INT);
+			}
+		}else{
+			printf("DATA_CHANGED(times)\n");
+		}
+	}
 }
 
 int main(void){
 	while(1){
-		printf("get a return = %d\n",getChoice());
+		printf("get a return = %d\n",new_getchoice());
 	}
 	return 0;
 }
 
-
-/*
-  note:
-  do not make a stupid mistake like this:
-  
-  char ch=getchar();
-  while(ch!='\n'){
-      ch=getchar();
-  }
-  
-  remember this:
-  
-  while(getchar()!='\n){
-      cotinue;
-  }
-*/
